3c910e7b16de8474a7080fd34236764798b2677b
Marouane Addou <105064179+marouaneaddou@users.noreply.github.com>
2026-09-23 00:23:31 +0000

    feat(View-Movie-Details): Add Trailer to Movie Details (#137)

--- src/main/java/com/filmexa/stream/modules/moviesExternal/dto/tmdb/trailer/Videos.java
+++ src/main/java/com/filmexa/stream/modules/moviesExternal/dto/tmdb/trailer/Videos.java
@@ -1,36 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   MovieDetailsResponse.java                          :+:      :+:    :+:   */
+/*   Videos.java                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: maddou <maddou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/09/17 23:31:37 by maddou            #+#    #+#             */
-/*   Updated: 2026/09/23 01:03:43 by maddou           ###   ########.fr       */
+/*   Created: 2026/09/23 00:52:17 by maddou            #+#    #+#             */
+/*   Updated: 2026/09/23 00:57:29 by maddou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-package com.filmexa.stream.modules.moviesExternal.dto.response;
-
-import lombok.Data;
+package com.filmexa.stream.modules.moviesExternal.dto.tmdb.trailer;
 
 import java.util.List;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
+@Data 
 @AllArgsConstructor
 @NoArgsConstructor
-public class MovieDetailsResponse {
-    Integer id;
-    String  backdropPath;
-    List< String > genres;
-    String overview;
-    String  releaseDate;
-    String title;
-    String imdbId;
-    Double rating;
-    String trailer;
-    List< ActorResponse > actors;
+public class Videos {
+    List< TrailerData> results;
 }
