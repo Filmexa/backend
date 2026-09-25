1f066713f66dadbe07d39503c3daebb84c7d67d2
Marouane Addou <105064179+marouaneaddou@users.noreply.github.com>
2026-09-25 19:59:46 +0000

    feat(Torrent #117): Handle errors

--- src/main/java/com/filmexa/stream/modules/torrent/client/YtsClient.java
+++ src/main/java/com/filmexa/stream/modules/torrent/client/YtsClient.java
@@ -6,7 +6,7 @@
 /*   By: marouan <marouan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/20 17:48:28 by marouan           #+#    #+#             */
-/*   Updated: 2026/09/24 14:06:58 by marouan          ###   ########.fr       */
+/*   Updated: 2026/09/25 20:56:12 by marouan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@ import java.util.List;
 import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientException;
 
 // import com.filmexa.stream.modules.torrent.dto.TorrentProviderResponseData;
 import com.filmexa.stream.modules.torrent.dto.yts.YtsResponseDto;
 import com.filmexa.stream.modules.torrent.dto.yts.YtsTorrentDto;
 import com.filmexa.stream.modules.torrent.dto.TorrentResultDto;
+import com.filmexa.stream.common.exception.ExternalServiceException;
 
 @Component("ytsClient")
 public class YtsClient implements TorrentClient {
@@ -34,28 +36,43 @@ public class YtsClient implements TorrentClient {
 
     @Override
     public List<TorrentResultDto> search( String imdbId ) {
-        // fetch data from yts torrent
-        YtsResponseDto response = restClient.get()
-            .uri("?imdb_id={imdId}", imdbId )
-            .retrieve()
-            .body( YtsResponseDto.class );
-        Integer id = response.getData().getMovie().getId();
-        List<YtsTorrentDto> dataYts =  response.getData().getMovie().getTorrents();
-        // map yts response to common client data
-        return dataYts.stream()
-            .map( torrent -> new TorrentResultDto(
-                id,
-                imdbId,
-                torrent.getHash(),
-                torrent.getSeeds(),
-                torrent.getPeers(),
-                torrent.getSize(),
-                torrent.getSize_bytes(),
-                torrent.getQuality(),
-                torrent.getType(),
-                torrent.getVideo_codec(),
-                null
-            ))
-            .toList();
+        try {
+            // fetch data from yts torrent
+            YtsResponseDto response = restClient.get()
+                .uri("?imdb_id={imdId}", imdbId )
+                .retrieve()
+                .body( YtsResponseDto.class );
+            if (response == null
+                || response.getData() == null
+                || response.getData().getMovie() == null) {
+                    throw new ExternalServiceException(
+                        "External service is unavailable"
+                    );
+            }
+            Integer id = response.getData().getMovie().getId();
+            List<YtsTorrentDto> dataYts =  response.getData().getMovie().getTorrents();
+            // map yts response to common client data
+            return dataYts.stream()
+                .map( torrent -> new TorrentResultDto(
+                    id,
+                    imdbId,
+                    torrent.getHash(),
+                    torrent.getSeeds(),
+                    torrent.getPeers(),
+                    torrent.getSize(),
+                    torrent.getSize_bytes(),
+                    torrent.getQuality(),
+                    torrent.getType(),
+                    torrent.getVideo_codec(),
+                    null
+                ))
+                .toList();
+        }
+        catch (RestClientException e) {
+            throw new ExternalServiceException(
+                    "External service is unavailable",
+                    e
+            );
+        }
     }
 }
